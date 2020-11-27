89b34aac2dff3c18a781f971093c17bc70799e51
demonvoyant <63505960+demonvoyant@users.noreply.github.com>
2020-11-27 19:34:05 +0200

    Add files via upload

--- bin.js
+++ bin.js
@@ -23,14 +23,18 @@ let statuses = {
         message: "❌ An invalid token was provided!"
     },
     cloneStatus: {
-        status: false,
+        status: true,
         message: ""
     }
 };
 
 async function checkServer (serverID) {
-    if (serverID === "") {
+    let server = client.guilds.get(serverID);
+
+    if (!server) {
         return false;
+    } else if (server.id === serverID){
+        return true;
     }
 }
 
@@ -76,34 +80,45 @@ app.use(bodyParser.json());
 app.post('/tryClone', async function(req, res){
     await checkToken(req.body.token);
 
-    if (checkToken(req.body.token)) {
+    if (checkToken(req.body.token) && checkServer(req.body.copyserv) && checkServer(req.body.pasteserv)) {
         let token = req.body.token
         let copy = req.body.copyserv
         let paste = req.body.pasteserv
 
         client.on("error", (e) => {
-            console.log(e)
             statuses.cloneStatus.status = false;
             statuses.cloneStatus.message = "❌ Error: " + e;
         });
 
         client.on("warn", (e) => {
-            console.log(e)
             statuses.cloneStatus.status = false;
             statuses.cloneStatus.message = "❌ Error: " + e;
         });
 
         (async function () {
-
         client.on("ready", async() => {
-            let guild1 = client.guilds.get(`${copy}`)
+            let guild1 = client.guilds.get(`${copy}`);
             let guild2 = client.guilds.get(`${paste}`);
+
+            if  (!guild1) {
+                statuses.cloneStatus.status=false;
+                return statuses.cloneStatus.message="❌ Clone server not found, or you're not in it.";
+            }
+
+            if  (!guild2) {
+                statuses.cloneStatus.status=false;
+                return statuses.cloneStatus.message="❌ Paste server not found, or you're not in it.";
+            } else if (!guild2.me.hasPermission("ADMINISTRATOR")) {
+                statuses.cloneStatus.status=false;
+                statuses.cloneStatus.message="❌ You don't have ADMINISTRATOR permissions in the paste server.";
+            }
 
             let channels = guild1.channels.filter(c => c.type === "text").sort((a, b) => a.calculatedPosition - b.calculatedPosition).map(c => c);
             let categories = guild1.channels.filter(c => c.type === "category").sort((a, b) => a.calculatedPosition - b.calculatedPosition).map(c => c);
             let roles = guild1.roles.sort((a, b) => b.calculatedPosition - a.calculatedPosition).map(r => r);
             let voice = guild1.channels.filter(c => c.type === "voice").sort((a, b) => a.calculatedPosition - b.calculatedPosition).map(c => c);
 
+            console.log(channels)
 
         let allowedRegions = ['brazil', 'us-west', 'singapore', 'eu-central', 'hongkong',
                             'us-south', 'amsterdam', 'us-central', 'london', 'us-east', 'sydney', 'japan',
@@ -114,7 +129,7 @@ app.post('/tryClone', async function(req, res){
             let guildname = guild1.name + " - cloned by SClone";
             let guildico = guild1.iconURL;
 
-            await guild2.channels.deleteAll();
+            guild2.channels.deleteAll();
 
             await guild2.setIcon(guildico)
             await guild2.setName(guildname)
@@ -122,7 +137,7 @@ app.post('/tryClone', async function(req, res){
             await guild2.setVerificationLevel(guild1.verificationLevel)
 
             for (var i = 0; i < roles.length; i++) {
-                let do2 = await guild2.roles.find(c => c.name === roles[i].name)
+                let do2 = guild2.roles.find(c => c.name === roles[i].name)
                 if (do2) continue;
                 await guild2.createRole({
                     type: roles[i].type,
@@ -143,21 +158,21 @@ app.post('/tryClone', async function(req, res){
             })
 
             categories.forEach(async(category) => {
-            let do2 = await guild2.channels.find(c => c.name === category.name)
-            if (do2) return;
+                let do2 = guild2.channels.find(c => c.name === category.name)
+                if (do2) return;
 
-            await guild2.createChannel(category.name, 'category')
+                await guild2.createChannel(category.name, 'category')
             })
 
                 for (var i = 0; i < channels.length; i++) {
                     let do1 = channels[i]
-                    let do2 = await guild2.channels.find(c => c.name === do1.name)
+                    let do2 = guild2.channels.find(c => c.name === do1.name)
                     if (do2) continue;
 
                     if (!do1.parent) {
                         var channel2 = await guild2.createChannel(do1.name, 'text')
                         if (channels[i].topic) {
-                            channel2.setTopic(channels[i].topic)
+                            await channel2.setTopic(channels[i].topic)
                         }
                     }
                     if (do1.parent) {
@@ -165,39 +180,42 @@ app.post('/tryClone', async function(req, res){
                         if (channels[i].topic) {
                             channel.setTopic(channels[i].topic)
                         }
-                        var ll = await guild2.channels.find(c => c.name === do1.parent.name)
+                        var ll = guild2.channels.find(c => c.name === do1.parent.name)
                         if (ll) {
-                            channel.setParent(guild2.channels.find(c => c.name === do1.parent.name).id)
+                            await channel.setParent(guild2.channels.find(c => c.name === do1.parent.name).id)
                         } else {
                             var ll1 = await guild2.createChannel(do1.parent.name, 'category')
-                            channel.setParent(ll1)
+                            await channel.setParent(ll1)
                         }
                     }
 
                 for (var i = 0; i < voice.length; i++) {
                     let do1 = voice[i]
-                    let do2 = await guild2.channels.find(c => c.name === do1.name)
+                    let do2 = guild2.channels.find(c => c.name === do1.name)
                     if (do2) continue;
                     if (!do1.parent) {
                         await guild2.createChannel(do1.name, 'voice')
                     }
                     if (do1.parent) {
                         var channel = await guild2.createChannel(do1.name, 'voice')
-                        var ll = await guild2.channels.find(c => c.name === do1.parent.name)
+                        var ll = guild2.channels.find(c => c.name === do1.parent.name)
                         if (ll) {
-                            channel.setParent(guild2.channels.find(c => c.name === do1.parent.name).id)
+                            await channel.setParent(guild2.channels.find(c => c.name === do1.parent.name).id)
                         } else {
                             var ll1 = await guild2.createChannel(do1.parent.name, 'category')
-                            channel.setParent(ll1)
+                            await channel.setParent(ll1)
                         }
                     }
                 }
             }
+            if  (statuses.cloneStatus.status) {
+                statuses.cloneStatus.status = true;
+                statuses.cloneStatus.message = "✅ Clone finished."
+            } else {
+                statuses.cloneStatus.status = false;
+                statuses.cloneStatus.message = "❌ Clone failed."
+            }
         })
-
-        statuses.cloneStatus.status = true;
-        statuses.cloneStatus.message = "✅ Clone finished."
-
         client.login(token).catch(()=>{ statuses.tokenStatus.status = false; statuses.tokenStatus.message = "❌ An invalid token was provided!"; });
         })();
     };
@@ -206,6 +224,11 @@ app.post('/tryClone', async function(req, res){
 });
 
 app.get('/getStatus', async function (req, res) {  
+    response = statuses;
+    await res.end(JSON.stringify(response));  
+});
+
+app.get('/getConsole', async function (req, res) {  
     response = statuses;
     await res.end(JSON.stringify(response));  
 });
--- index.js
+++ index.js
@@ -2,7 +2,25 @@ const { exec } = require("child_process");
 
 console.log("Do not close this window, it is used to keep the backend server running, used for Discord's API and also it acts as a De-bug console.")
 
-exec("electron ./main.js", (error, stdout, stderr) => {
+const { accessSync } = require("fs");
+const path = require("path");
+
+try {
+    accessSync(path.join(__dirname, '..', 'node_modules', "electron")); //see if node_modules folder exists
+} catch (e) {
+  exec("npm i electron", (error, stdout, stderr) => {
+    if (error) {
+        console.log(`error: ${error.message}`);
+        return;
+    }
+    if (stderr) {
+        console.log(`stderr: ${stderr}`);
+        return;
+    }
+  });
+}
+
+exec("npx electron ./main.js", (error, stdout, stderr) => {
   if (error) {
       console.log(`error: ${error.message}`);
       return;
@@ -12,5 +30,16 @@ exec("electron ./main.js", (error, stdout, stderr) => {
       return;
   }
 });
+
+/*exec("node ./bin.js > console.log", (error, stdout, stderr) => {
+  if (error) {
+      console.log(`error: ${error.message}`);
+      return;
+  }
+  if (stderr) {
+      console.log(`stderr: ${stderr}`);
+      return;
+  }
+});*/
 
 require("./bin.js");
